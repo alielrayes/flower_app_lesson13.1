6583a26f3f6e58e30b7cf4d76b83d34cf5556809
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-05-24 21:06:46 +0200

    first commit

---
+++ lib/pages/register.dart
@@ -0,0 +1,186 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sort_child_properties_last
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/shared/colors.dart';
+import 'package:flower_app/shared/contants.dart';
+import 'package:flower_app/shared/snackbar.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:email_validator/email_validator.dart';
+
+class Register extends StatefulWidget {
+  Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  bool isVisable = true;
+
+  final _formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  register() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        showSnackBar(context, "The password provided is too weak.");
+      } else if (e.code == 'email-already-in-use') {
+        showSnackBar(context, "The account already exists for that email.");
+      } else {
+        showSnackBar(context, "ERROR - Please try again late");
+      }
+    } catch (err) {
+      showSnackBar(context, err.toString());
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    TextField(
+                        keyboardType: TextInputType.text,
+                        obscureText: false,
+                        decoration: decorationTextfield.copyWith(
+                            hintText: "Enter Your username : ",
+                            suffixIcon: Icon(Icons.person))),
+                    const SizedBox(
+                      height: 33,
+                    ),
+                    TextFormField(
+                        // we return "null" when something is valid
+                        validator: (value) {
+                          return value != null &&
+                                  !EmailValidator.validate(value)
+                              ? "Enterrrr a valid email"
+                              : null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        obscureText: false,
+                        decoration: decorationTextfield.copyWith(
+                            hintText: "Enter Your Email : ",
+                            suffixIcon: Icon(Icons.email))),
+                    const SizedBox(
+                      height: 33,
+                    ),
+                    TextFormField(
+                        // we return "null" when something is valid
+                        validator: (value) {
+                          return value!.length < 8
+                              ? "Enter at least 8 characters"
+                              : null;
+                        },
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: passwordController,
+                        keyboardType: TextInputType.text,
+                        obscureText: isVisable ? true : false,
+                        decoration: decorationTextfield.copyWith(
+                            hintText: "Enter Your Password : ",
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isVisable = !isVisable;
+                                  });
+                                },
+                                icon: isVisable?  Icon(Icons.visibility)  : Icon(Icons.visibility_off)
+                                
+                                
+                                ))),
+                    const SizedBox(
+                      height: 33,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          register();
+                        } else {
+                          showSnackBar(context, "ERROR");
+                        }
+                      },
+                      child: isLoading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Register",
+                              style: TextStyle(fontSize: 19),
+                            ),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(BTNgreen),
+                        padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8))),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 33,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Do not have an account?",
+                            style: TextStyle(fontSize: 18)),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Login()),
+                              );
+                            },
+                            child: Text('sign in',
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 18))),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
